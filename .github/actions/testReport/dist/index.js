67d2cd451a3ee586957346ee9b24811c2e8430cc
Jarosław Ciarkowski <jaroslaw.ciarkowski@antologic.com>
2022-04-20 14:25:23 +0200

    fixed?

--- .github/actions/testReport/dist/index.js
+++ .github/actions/testReport/dist/index.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
 const TestReport_1 = require("./TestReport");
-const inputName = (0, core_1.getInput)("path");
-generateReport(inputName);
-async function generateReport(path) {
-    console.log(`'Path to report xml files: ${path}!'`);
-    let html = (0, TestReport_1.formatHtmlReport)("localhost:8080", path, "header1", "header2", "header3", ["1", "2", "3"]);
+const heroldUrl = (0, core_1.getInput)("heroldUrl");
+const path = (0, core_1.getInput)("path");
+const header1 = (0, core_1.getInput)("header1");
+const header2 = (0, core_1.getInput)("header2");
+const header3 = (0, core_1.getInput)("header3");
+const failedTestProjects = (0, core_1.getInput)("failedTestProjects");
+generateReport(heroldUrl, path, header1, header2, header3, failedTestProjects);
+function generateReport(heroldUrl, path, header1, header2, header3, failedTestProjects) {
+    console.log(`'Path to report xml files: ${path}'`);
+    let html = (0, TestReport_1.formatHtmlReport)(heroldUrl, path, header1, header2, header3, failedTestProjects.split(","));
     console.log(html);
 }
